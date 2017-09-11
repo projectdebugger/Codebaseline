fcaafc9305d556d0fce08c822a7762f99b2d5174
santoshbista02 <santoshbista02@gmail.com>
2017-09-11 20:40:42 +1000

    Barcode UI

--- BCCCP/src/bcccp/carpark/paystation/PaystationController.java
+++ BCCCP/src/bcccp/carpark/paystation/PaystationController.java
@@ -47,11 +47,11 @@ return (ui + " " + carpark);
 	public void receiveDetections(Detector.Detections<Barcode> barcode) {
                 //final SparseArray<Barcode> barcode = detections.getDetectedItems();
                 if (barcodes.size() != 0) {
-                    String data = barcode.valueAt(0).displayValue;
+                    String data = barcode.valueAt(0).ui;
 
-                    Log.d(TAG, "Barcode detected: " + data);
+                    //Log.d(TAG, "Barcode detected: " + data);
                     this.adhocTicket.enter(new Date().getTime());
-                    //this.ui.beep();
+                    this.ui.beep();
                     this.charge();
 
                     return barcode;
