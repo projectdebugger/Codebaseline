a096a477ed75dedc4dc03d3d3ff5638495f6337d
cuonghoang1504 <cuongit.uet@gmail.com>
2017-09-18 00:40:38 +1000

    Method added
    A method added to change the status of car sensor.

--- BCCCP/src/bcccp/carpark/ICarSensor.java
+++ BCCCP/src/bcccp/carpark/ICarSensor.java
@@ -3,7 +3,7 @@ package bcccp.carpark;
 public interface ICarSensor {
 	public void registerResponder(ICarSensorResponder responder);
 	public void deregisterResponder(ICarSensorResponder responder);
-	
+	public void turnDetectorStutus(boolean status);
 	public String getId();
 	public boolean carIsDetected();
 
